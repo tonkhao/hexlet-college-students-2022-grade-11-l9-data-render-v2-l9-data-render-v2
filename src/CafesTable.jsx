43780b3296f83926f06a3405073fcc2544346bda
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-12-23 14:48:23 +0300

    add axios endpoint for getting data

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react';
+import axios from "axios";
 import FilterCafes from "./FilterCafes";
 
 
 export default function CafesTable() {
-    return <div class = 'cafesTable'>
+    useEffect(async () => {
+        const cafes = await axios.get('http://localhost:8070/cafes');
+        console.log(cafes);
+        return () => {
+            ;s
+        };
+    }, []);
+    return <div class='cafesTable'>
         <FilterCafes />
         <ul class="cardsList"></ul>
     </div>
