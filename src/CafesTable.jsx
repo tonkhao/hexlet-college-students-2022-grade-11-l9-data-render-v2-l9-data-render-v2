fc7b005b2a7d65ebb0793f80cdcc19c3572fabe4
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-12-24 13:01:46 +0300

    code ready

--- src/CafesTable.jsx
+++ src/CafesTable.jsx
@@ -4,29 +4,46 @@ import FilterCafes from "./FilterCafes";
 
 
 export default function CafesTable() {
+    const [initialCafeState, setInitialCafes] = useState([]);
     const [cafes, setCafes] = useState([]);
     useEffect(() => {
         const fetchCafes = async () => {
             try {
                 const result = await axios.get('http://localhost:8070/cafes');
                 setCafes(result.data.cafes);
+                setInitialCafes(result.data.cafes)
+                console.log(initialCafeState)
             } catch (error) {
                 console.error("Error fetching cafes:", error);
             }
         };
         fetchCafes();
     }, []);
+    const getCafeImg = (img) => {
+        if (!img) {
+            return "https://via.placeholder.com/150"
+        }
+        return img
+    }
+    const onHandleFilterChange = (subwayCode) => {
+        if (subwayCode === "All") {
+            setCafes(initialCafeState)
+            return
+        }
+        const filteredCafes = initialCafeState.filter(cafe => cafe.subwayCode === subwayCode)
+        setCafes(filteredCafes)
+    }
     if (cafes.length === 0) return <div>Loading...</div>;
     return <div className='cafesTable'>
-        <FilterCafes />
+        <FilterCafes cafes={initialCafeState} onHandleFilterChange={onHandleFilterChange} />
         <ul className="cardsList">
             {cafes.map(cafe => {
                 return <li className="card">
-                    <img src={cafe.img} alt={cafe.code} />
+                    <img src={getCafeImg(cafe.img)} alt={cafe.code} />
                     <h2>{cafe.name}</h2>
                     <p>{cafe.decs}</p>
                     <p>{cafe.address}</p>
-                    <p>{cafe.code}</p>
+                    <p>{cafe.subwayCode}</p>
                     <p>{cafe.workTime}</p>
                 </li>
             })}
--- src/FilterCafes.jsx
+++ src/FilterCafes.jsx
@@ -1,7 +1,13 @@
-export default function FilterCafes() {
+export default function FilterCafes(props) {
+    const handleChangeFilter = (e) => {
+        props.onHandleFilterChange(e.target.value)
+    }
     return <div className="controls">
-        <select name="subway" id="subway">
+        <select name="subway" id="subway" onChange={handleChangeFilter}>
             <option value="All">Все</option>
+            {props.cafes.map(cafe => {
+                return <option value={cafe.subwayCode}>{cafe.subwayCode}</option>
+            })}
         </select>
     </div>
 }
